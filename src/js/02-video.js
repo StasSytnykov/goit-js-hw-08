2e91abdae23aca19054ffabb63c27334b4604e01
Stas Sytnykov <stas82kak05@gmail.com>
2022-01-26 17:30:07 +0200

    fixed vimeo player

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -4,13 +4,9 @@ const iframe = document.querySelector('iframe');
 const player = new Vimeo.Player(iframe);
 const LOCALSTORAGE_KEY = 'videoplayer-current-time';
 
-player.on('play', function () {
-  console.log('played the video!');
-});
+player.on('play', function () {});
 
-player.getVideoTitle().then(function (title) {
-  console.log('title:', title);
-});
+player.getVideoTitle().then(function (title) {});
 
 const onPlay = _.throttle(function (data) {
   localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(data));
@@ -20,10 +16,10 @@ player.on('timeupdate', onPlay);
 
 const currentTime = localStorage.getItem(LOCALSTORAGE_KEY);
 const parsedCurrentTime = JSON.parse(currentTime);
-console.log(parsedCurrentTime);
+const parsedCurrentSeconds = parsedCurrentTime !== null ? parsedCurrentTime.seconds : 0;
 
 player
-  .setCurrentTime(parsedCurrentTime.seconds)
+  .setCurrentTime(parsedCurrentSeconds)
   .then(function (seconds) {
     // seconds = the actual time that the player seeked to
   })
