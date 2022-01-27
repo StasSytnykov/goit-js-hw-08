62156a8cd4865a84a7963972ee13c58642f18741
Stas Sytnykov <stas82kak05@gmail.com>
2022-01-27 18:30:48 +0200

    refactoring code and add nandlebars

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,18 +1,22 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
-import SimpleLightbox from 'simplelightbox'
+import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
+const Handlebars = require('handlebars');
 
 const galleryList = document.querySelector('.gallery');
+const makeGallery = onMakeGalleryMarkup(galleryItems);
 
-const makeGalleryMarkup = ({ preview, original, description }) => {
-  return `<a class="gallery__item" href="${original}">
+function onMakeGalleryMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<a class="gallery__item" href="${original}">
   <img class="gallery__image" src="${preview}" alt="${description}" title="${description}" />
 </a>`;
-};
-
-const makeGallery = galleryItems.map(makeGalleryMarkup).join('');
+    })
+    .join('');
+}
 
 galleryList.innerHTML += makeGallery;
 
@@ -22,3 +26,13 @@ let gallery = new SimpleLightbox('.gallery a', {
 gallery.on('show.simplelightbox', function () {
   // do something…
 });
+
+{
+  {
+    galleryItems.preview;
+  }
+}
+// compile the template
+const template = Handlebars.compile('Handlebars <b>{{galleryItems.preview}} {{bla}}</b>');
+// execute the compiled template and print the output to the console
+console.log(template({ galleryItems: 'galleryItems.preview' }));
